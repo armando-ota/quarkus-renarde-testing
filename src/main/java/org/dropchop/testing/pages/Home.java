5ce3b2a640fddf498acc878fd8985134173ee5d1
Armando Ota <armando.ota@dropchop.com>
2024-06-02 21:13:31 +0200

    Figuring out how to include templated dynamically.

--- src/main/java/org/dropchop/testing/pages/Home.java
+++ src/main/java/org/dropchop/testing/pages/Home.java
@@ -5,16 +5,19 @@ import io.quarkus.qute.CheckedTemplate;
 import io.quarkus.qute.TemplateInstance;
 import jakarta.ws.rs.Path;
 
+import java.util.List;
+
 public class Home extends Controller {
 
   @CheckedTemplate
   static class Templates {
-    public static native TemplateInstance index();
+    public static native TemplateInstance index(List<String> names);
   }
 
   @Path("/")
   public TemplateInstance index() {
-    return Templates.index();
+    List<String> names = List.of("Component1/index","Component2/index","Component3/index");
+    return Templates.index(names);
   }
 
 }
